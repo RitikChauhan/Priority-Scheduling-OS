d9e47040d5924d34df4b4a56b315a2c55e5c3248
RitikChauhan <49956465+RitikChauhan@users.noreply.github.com>
2019-04-24 23:03:57 +0530

    Update os.c

--- os.c
+++ os.c
@@ -5,8 +5,8 @@ void main()
   char p[10][5],temp[5];
   int i,j,pt[10],wt[10],totwt=0,pr[10],temp1,n;
   float avgwt;
- printf("enter no of processes:");
- scanf("%d",&n);
+  printf("enter no of processes:");
+  scanf("%d",&n);
   for(i=0;i<n;i++)
   {
   printf("enter process%d name:",i+1);
